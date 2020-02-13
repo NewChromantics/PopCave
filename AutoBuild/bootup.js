039c334a6c1d4d2df84ce78b32b11a7aca09599d
Graham Reeves <graham@grahamreeves.com>
2020-02-13 13:01:51 +0000

    Now building zip from artifacts!

--- AutoBuild/bootup.js
+++ AutoBuild/bootup.js
@@ -10,7 +10,8 @@ Pop.Perforce.WaitForRevisionPush = async function(WorkspacePath)
 async function BuildUnreal(Path,OnStdOut)
 {
 	//const Command = "-ScriptsForProject=F:/vin/Vignettes.uproject BuildCookRun -nocompile -nocompileeditor -installed -nop4 -project=F:/vin/Vignettes.uproject -cook -stage -archive -archivedirectory=F:/vin/Build -package -clientconfig=Shipping -ue4exe="D:\Epic Games\UE_4.23\Engine\Binaries\Win64\UE4Editor-Cmd.exe" -pak -prereqs -nodebuginfo -targetplatform=Win64 -build -utf8output";
-	const Command = "notepad.exe";
+	//const Command = "dir .";
+	const Command = "D:\\Epic Games\\UE_4.23\\Engine\\Build\\BatchFiles\\RunUAT.bat";
 
 	//	create process
 	const Process = new Pop.ShellExecute(Command);
@@ -22,7 +23,7 @@ async function BuildUnreal(Path,OnStdOut)
 		{
 			//	this will return an exit code when process has exited
 			const NewStdOutput = await Process.WaitForOutput();
-			Pop.Debug("Output",NewStdOutput,typeof NewStdOutput);
+			//Pop.Debug("Output",NewStdOutput,typeof NewStdOutput);
 			if (typeof NewStdOutput == "number")
 			{
 				const ExitCode = NewStdOutput;
@@ -36,12 +37,22 @@ async function BuildUnreal(Path,OnStdOut)
 	return ExitCode;
 }
 
-async function BuildWorkspace(Path,OnBuildMetaUpdate,FilterArtifactFilename)
+function String_CutLeft(Haystack,Needle)
 {
-	const RevisionNumber = await Pop.Perforce.WaitForRevisionPush(Path);
+	//	while to allow trim of multiple starting chars/strings
+	while (Haystack.startsWith(Needle))
+	{
+		Haystack = Haystack.slice(Needle.length);
+	}
+	return Haystack;
+}
+
+async function BuildWorkspace(InputMeta,OnBuildMetaUpdate)
+{
+	const RevisionNumber = await Pop.Perforce.WaitForRevisionPush(InputMeta.Path);
 
 	const BuildMeta = {};
-	BuildMeta.Path = Path;
+	BuildMeta.Path = InputMeta.Path;
 	BuildMeta.RevisionNumber = RevisionNumber;
 	BuildMeta.StartTimeMs = Pop.GetTimeNowMs();
 	BuildMeta.StdOut = "";
@@ -50,23 +61,44 @@ async function BuildWorkspace(Path,OnBuildMetaUpdate,FilterArtifactFilename)
 	//	start build process
 	function OnStdOut(Output)
 	{
+		Output = Output.replace('\n','\\n');
+		Output = Output.replace('\r','\\r');
 		BuildMeta.StdOut += Output;
 		OnBuildMetaUpdate(BuildMeta);
 	}
 
-	const ExitCode = await BuildUnreal(Path,OnStdOut);
+	const ExitCode = await BuildUnreal(InputMeta.Path,OnStdOut);
 	BuildMeta.ExitCode = ExitCode;
 	OnBuildMetaUpdate(BuildMeta);
 
 	//	create zip of artifacts
 	//	enum every file in path, and filter
-	const AllFilenames = Pop.GetFilenames(Path);
-	const ArtifactFilenames = AllFilenames.filter(FilterArtifactFilename);
+	const AllFilenames = Pop.GetFilenames(InputMeta.Path);
+	const ArtifactFilenames = AllFilenames.filter(InputMeta.FilterArtifactFilename);
 
 	BuildMeta.ArtifactFilenames = ArtifactFilenames;
 	OnBuildMetaUpdate(BuildMeta);
 
-	//	save zip somewhere
+	BuildMeta.Zip = {};
+	BuildMeta.Zip.StartTimeMs = Pop.GetTimeNowMs();
+	BuildMeta.Zip.AddedFiles = [];
+	BuildMeta.Zip.Filename = InputMeta.ZipFilename;
+
+	const Zip = new Pop.Zip.Archive(InputMeta.ZipFilename);
+	for (const Filename of BuildMeta.ArtifactFilenames)
+	{
+		let ZipFilename = String_CutLeft(Filename,InputMeta.Path);
+		ZipFilename = ZipFilename.replace(/\\/g,"/");
+		ZipFilename = String_CutLeft(ZipFilename,"/");
+		await Zip.AddFile(Filename,ZipFilename);
+		Pop.Debug("Added " + ZipFilename,Filename);
+		BuildMeta.Zip.AddedFiles.push(ZipFilename);
+		OnBuildMetaUpdate(BuildMeta);
+	}
+	Zip.Close();
+
+	Pop.ShowFileInFinder(BuildMeta.Zip.Filename);
+	BuildMeta.EndTimeMs = Pop.GetTimeNowMs();
 
 	return BuildMeta;
 }
@@ -76,7 +108,7 @@ async function AutoBuild(Path)
 {
 	function OnBuildMetaUpdate(BuildMeta)
 	{
-		//Pop.Debug("Build Meta updated");
+		Pop.Debug("Build Meta updated");
 	}
 
 	function FilterArtifactFilename(Filename)
@@ -90,8 +122,14 @@ async function AutoBuild(Path)
 	//while (true)
 	{
 		Pop.Debug("Waiting for next build...");
-		const BuildMeta = await BuildWorkspace(Path,OnBuildMetaUpdate,FilterArtifactFilename);
-		Pop.Debug("Build finished",JSON.stringify(BuildMeta));
+		const BuildInfo = {};
+		BuildInfo.ZipFilename = 'Build.zip';
+		BuildInfo.FilterArtifactFilename = FilterArtifactFilename;
+		BuildInfo.Path = Path;
+
+		const BuildMeta = await BuildWorkspace(BuildInfo,OnBuildMetaUpdate);
+		const DurationSecs = (BuildMeta.EndTimeMs - BuildMeta.StartTimeMs)/1000;
+		Pop.Debug(`Build finished after ${DurationSecs} secs`,JSON.stringify(BuildMeta,null,'\t'));
 
 		await Pop.Yield(1000);
 	}
